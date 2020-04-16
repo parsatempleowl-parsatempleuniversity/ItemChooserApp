9b0418c0931d6061cf967ac480e32b6cf9ae25df
Parsa Temple Owl <tug81042@temple.edu>
2020-04-16 14:51:36 -0400

    completed the MainActivity

--- app/src/main/java/com/example/itemchooserapp/MainActivity.java
+++ app/src/main/java/com/example/itemchooserapp/MainActivity.java
@@ -6,9 +6,33 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean singleContainer;
+    CanvasFragment canvasFragment = new CanvasFragment();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] items = {"Blu-Ray: Star Trek: The Motion Picture Collection (All 6 Movies)", "Blu-Ray: The Boy Who Cried Werewolf: Nickelodeon",
+                "Blu-Ray: Real Steel", "Blu-Ray: BumbleBee", "Blu-Ray: Wimbledon", "Blu-Ray: Family Double Feature: A Cinderella Story, Another Cinderella Story",
+                "Blu-Ray: Star Trek", "Blu-Ray: She’s Out Of My League", "Blu-Ray: Fast & Furious: 7-Movie Collection", "Blu-Ray: The Hunger Games: Complete 4-Film Collection",
+                "Blu-Ray: Transformers: 5-Movie Collection", "Blu-Ray: Frozen: Disney", "Blu-Ray: Fifty Shades: 3-Movie Collection", "Blu-Ray: Twilight", "Blu-Ray: Transformers: 2-Movie Collection"};
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container_1, PaletteFragment.newInstance(items)).addToBackStack(null).commit();
+        singleContainer = findViewById(R.id.container_2) == null;
+        if(!singleContainer) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container_2, canvasFragment).addToBackStack(null).commit();
+        }
+    }
+
+    @Override
+    public void chosenItem(String item) {
+        if(singleContainer) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container_1, CanvasFragment.newInstance(item)).addToBackStack(null).commit();
+        }
+        else {
+            canvasFragment.displayItemInformation(item);
+        }
+    }
     }
 }
