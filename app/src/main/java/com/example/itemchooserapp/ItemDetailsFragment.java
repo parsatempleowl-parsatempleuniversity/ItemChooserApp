4d71c55b3ff3b79f95e5e443bb571930b320452b
Parsa Temple Owl <tug81042@temple.edu>
2020-04-17 20:35:54 -0400

    made changes to ItemDetailsFragment, made changes to strings.xml, made changes to portrait orientaion layout for ItemDetailsFragment, and made changes to landscape orientation layout for ItemDetailsFragment

--- app/src/main/java/com/example/itemchooserapp/ItemDetailsFragment.java
+++ app/src/main/java/com/example/itemchooserapp/ItemDetailsFragment.java
@@ -51,131 +51,103 @@ public class ItemDetailsFragment extends Fragment {
 
     void displayItemInformation(String item) {
         ImageView imageView;
+        imageView = view.findViewById(R.id.imageView);
         TextView textView1;
+        textView1 = view.findViewById(R.id.textView1);
         TextView textView2;
+        textView2 = view.findViewById(R.id.textView2);
         TextView textView3;
+        textView3 = view.findViewById(R.id.textView3);
         switch (item) {
             case "Blu-Ray: Star Trek: The Motion Picture Collection (All 6 Movies)":
-                imageView = view.findViewById(R.id.imageView);
                 imageView.setImageResource(R.drawable.image1);
-                textView1 = view.findViewById(R.id.textView1);
                 textView1.setText(R.string.price1);
-                textView2 = view.findViewById(R.id.textView2);
                 textView2.setText(R.string.pricePlusTax1);
-                textView3 = view.findViewById(R.id.textView3);
-                textView3.setText("Blu-Ray: Star Trek: The Motion Picture Collection (All 6 Movies)");
+                textView3.setText(R.string.item1);
                 break;
             case "Blu-Ray: The Boy Who Cried Werewolf: Nickelodeon":
-                imageView = view.findViewById(R.id.imageView);
                 imageView.setImageResource(R.drawable.image2);
-                textView1 = view.findViewById(R.id.textView1);
                 textView1.setText(R.string.price2);
-                textView2 = view.findViewById(R.id.textView2);
                 textView2.setText(R.string.pricePlusTax2);
+                textView3.setText(R.string.item2);
                 break;
             case "Blu-Ray: Real Steel":
-                imageView = view.findViewById(R.id.imageView);
                 imageView.setImageResource(R.drawable.image3);
-                textView1 = view.findViewById(R.id.textView1);
                 textView1.setText(R.string.price2);
-                textView2 = view.findViewById(R.id.textView2);
                 textView2.setText(R.string.pricePlusTax2);
+                textView3.setText(R.string.item3);
                 break;
             case "Blu-Ray: BumbleBee":
-                imageView = view.findViewById(R.id.imageView);
                 imageView.setImageResource(R.drawable.image4);
-                textView1 = view.findViewById(R.id.textView1);
                 textView1.setText(R.string.price2);
-                textView2 = view.findViewById(R.id.textView2);
                 textView2.setText(R.string.pricePlusTax2);
+                textView3.setText(R.string.item4);
                 break;
             case "Blu-Ray: Wimbledon":
-                imageView = view.findViewById(R.id.imageView);
                 imageView.setImageResource(R.drawable.image5);
-                textView1 = view.findViewById(R.id.textView1);
                 textView1.setText(R.string.price2);
-                textView2 = view.findViewById(R.id.textView2);
                 textView2.setText(R.string.pricePlusTax2);
+                textView3.setText(R.string.item5);
                 break;
             case "Blu-Ray: Family Double Feature: A Cinderella Story, Another Cinderella Story":
-                imageView = view.findViewById(R.id.imageView);
                 imageView.setImageResource(R.drawable.image6);
-                textView1 = view.findViewById(R.id.textView1);
                 textView1.setText(R.string.price3);
-                textView2 = view.findViewById(R.id.textView2);
                 textView2.setText(R.string.pricePlusTax3);
+                textView3.setText(R.string.item6);
                 break;
             case "Blu-Ray: Star Trek":
-                imageView = view.findViewById(R.id.imageView);
                 imageView.setImageResource(R.drawable.image7);
-                textView1 = view.findViewById(R.id.textView1);
                 textView1.setText(R.string.price2);
-                textView2 = view.findViewById(R.id.textView2);
                 textView2.setText(R.string.pricePlusTax2);
+                textView3.setText(R.string.item7);
                 break;
             case "Blu-Ray: She’s Out Of My League":
-                imageView = view.findViewById(R.id.imageView);
                 imageView.setImageResource(R.drawable.image8);
-                textView1 = view.findViewById(R.id.textView1);
                 textView1.setText(R.string.price2);
-                textView2 = view.findViewById(R.id.textView2);
                 textView2.setText(R.string.pricePlusTax2);
+                textView3.setText(R.string.item8);
                 break;
             case "Blu-Ray: Fast & Furious: 7-Movie Collection":
-                imageView = view.findViewById(R.id.imageView);
                 imageView.setImageResource(R.drawable.image9);
-                textView1 = view.findViewById(R.id.textView1);
                 textView1.setText(R.string.price4);
-                textView2 = view.findViewById(R.id.textView2);
                 textView2.setText(R.string.pricePlusTax4);
+                textView3.setText(R.string.item9);
                 break;
             case "Blu-Ray: The Hunger Games: Complete 4-Film Collection":
-                imageView = view.findViewById(R.id.imageView);
                 imageView.setImageResource(R.drawable.image10);
-                textView1 = view.findViewById(R.id.textView1);
                 textView1.setText(R.string.price5);
-                textView2 = view.findViewById(R.id.textView2);
                 textView2.setText(R.string.pricePlusTax5);
+                textView3.setText(R.string.item10);
                 break;
             case "Blu-Ray: Transformers: 5-Movie Collection":
-                imageView = view.findViewById(R.id.imageView);
                 imageView.setImageResource(R.drawable.image11);
-                textView1 = view.findViewById(R.id.textView1);
                 textView1.setText(R.string.price6);
-                textView2 = view.findViewById(R.id.textView2);
                 textView2.setText(R.string.pricePlusTax6);
+                textView3.setText(R.string.item11);
                 break;
             case "Blu-Ray: Frozen: Disney":
-                imageView = view.findViewById(R.id.imageView);
                 imageView.setImageResource(R.drawable.image12);
-                textView1 = view.findViewById(R.id.textView1);
                 textView1.setText(R.string.price2);
-                textView2 = view.findViewById(R.id.textView2);
                 textView2.setText(R.string.pricePlusTax2);
+                textView3.setText(R.string.item12);
                 break;
             case "Blu-Ray: Fifty Shades: 3-Movie Collection":
-                imageView = view.findViewById(R.id.imageView);
                 imageView.setImageResource(R.drawable.image13);
-                textView1 = view.findViewById(R.id.textView1);
                 textView1.setText(R.string.price7);
-                textView2 = view.findViewById(R.id.textView2);
                 textView2.setText(R.string.pricePlusTax7);
+                textView3.setText(R.string.item13);
                 break;
             case "Blu-Ray: Twilight":
-                imageView = view.findViewById(R.id.imageView);
                 imageView.setImageResource(R.drawable.image14);
-                textView1 = view.findViewById(R.id.textView1);
                 textView1.setText(R.string.price2);
-                textView2 = view.findViewById(R.id.textView2);
                 textView2.setText(R.string.pricePlusTax2);
+                textView3.setText(R.string.item14);
                 break;
             case "Blu-Ray: Transformers: 2-Movie Collection":
-                imageView = view.findViewById(R.id.imageView);
                 imageView.setImageResource(R.drawable.image15);
-                textView1 = view.findViewById(R.id.textView1);
                 textView1.setText(R.string.price3);
-                textView2 = view.findViewById(R.id.textView2);
                 textView2.setText(R.string.pricePlusTax3);
+                textView3.setText(R.string.item15);
                 break;
         }
     }
